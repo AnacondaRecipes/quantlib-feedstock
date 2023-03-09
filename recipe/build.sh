40cbc1900a3caf28985adc35d19d85a937e7d19c
Serhii Kupriienko <skupriienko@anaconda.com>
2023-03-09 16:19:33 +0200

    Move tests to commands on unix; debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,18 +21,21 @@ cmake .. ${CMAKE_ARGS} \
 echo "Building..."
 ninja -j${CPU_COUNT} || exit 1
 
+ls test-suite
+
 # Perform tests.
-echo "Testing..."
-# quantlib_test_suite fails on linux-s390x and linux-aarch64.
-if [[ "$(uname -m)" == aarch64 ]]; then
-    echo Skip the following tests on aarch64.
-elif [[ "$(uname -m)" == s390x ]]; then
-    echo Skip the following tests on s390x.
-else 
-    ninja test || exit 1
-    ./test-suite/quantlib-test-suite --log_level=message || exit 1
-    ctest -VV --output-on-failure || exit 1
-fi
+# echo "Testing..."
+# # quantlib_test_suite fails on linux-s390x and linux-aarch64.
+# if [[ "$(uname -m)" == aarch64 ]]; then
+#     echo Skip the following tests on aarch64.
+# elif [[ "$(uname -m)" == s390x ]]; then
+#     echo Skip the following tests on s390x.
+# else 
+#     ninja test || exit 1
+#     ./test-suite/quantlib-test-suite --log_level=message || exit 1
+#     ctest -VV --output-on-failure || exit 1
+# fi
+
 
 # Installing
 echo "Installing..."
